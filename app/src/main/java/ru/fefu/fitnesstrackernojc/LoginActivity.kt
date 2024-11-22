df49d704c875cbba330da32daa6bef7c7bbf74d7
Alexey Streltsov <streltcov.ar@dvfu.ru>
2024-11-23 00:25:53 +1000

    Initial commit

---
+++ app/src/main/java/ru/fefu/fitnesstrackernojc/LoginActivity.kt
@@ -0,0 +1,6 @@
+package ru.fefu.fitnesstrackernojc
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+}
---
+++ app/src/main/java/ru/fefu/fitnesstrackernojc/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package ru.fefu.fitnesstrackernojc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
