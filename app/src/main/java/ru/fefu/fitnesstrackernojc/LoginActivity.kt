66d26c5b6712301bbe383c307c59b1c22dced129
Alexey Streltsov <streltcov.ar@dvfu.ru>
2024-11-23 17:53:33 +1000

    release версия приложения

--- app/src/main/java/ru/fefu/fitnesstrackernojc/LoginActivity.kt
+++ app/src/main/java/ru/fefu/fitnesstrackernojc/LoginActivity.kt
@@ -3,11 +3,16 @@ package ru.fefu.fitnesstrackernojc
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Кнопку "Назад"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
--- app/src/main/java/ru/fefu/fitnesstrackernojc/RegisterActivity.kt
+++ app/src/main/java/ru/fefu/fitnesstrackernojc/RegisterActivity.kt
@@ -3,11 +3,14 @@ package ru.fefu.fitnesstrackernojc
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
