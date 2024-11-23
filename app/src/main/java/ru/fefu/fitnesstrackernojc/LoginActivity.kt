012de0d8cf1e52e0b7e18a724cd53f7876b97ef9
Alexey Streltsov <streltcov.ar@dvfu.ru>
2024-11-23 17:01:00 +1000

    0.1 версия готового приложения

--- app/src/main/java/ru/fefu/fitnesstrackernojc/LoginActivity.kt
+++ app/src/main/java/ru/fefu/fitnesstrackernojc/LoginActivity.kt
@@ -1,6 +1,22 @@
 package ru.fefu.fitnesstrackernojc
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finish()
+        return true
+    }
+
 }
--- app/src/main/java/ru/fefu/fitnesstrackernojc/RegisterActivity.kt
+++ app/src/main/java/ru/fefu/fitnesstrackernojc/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package ru.fefu.fitnesstrackernojc
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +8,14 @@ class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finish()
+        return true
     }
 }
